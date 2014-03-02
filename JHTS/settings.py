c92e2c9c3b719c7da537fcb393e93bba9d4c94c1
Anurag <anssharma61@gmail.com>
2014-03-02 19:24:13 +0530

    reninstalled ckeditor

--- JHTS/settings.py
+++ JHTS/settings.py
@@ -92,6 +92,11 @@ MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'ck_media')
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 CKEDITOR_IMAGE_BACKEND = "pillow"
+CKEDITOR_CONFIGS = {
+    'default':{
+    'toolbar': 'Advanced',
+    }
+}
 
 # Notice board config
 
