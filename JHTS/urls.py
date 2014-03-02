62f9b32f812f455a082410810cd273b9c4be0c81
Anurag <anssharma61@gmail.com>
2014-03-02 20:36:42 +0530

    making the auth system, need some checks for login in the template

--- JHTS/urls.py
+++ JHTS/urls.py
@@ -8,6 +8,10 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog', include('main.urls')),
-    url(r'^', include('pages.urls')),
+    url(r'^login/$', 'JHTS.views.loginview'),
+	url(r'^auth/$', 'JHTS.views.auth_and_login'),
+    url(r'^$', include('pages.urls')),
     url(r'^ckeditor/', include('ckeditor.urls')),
+    
+	
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
