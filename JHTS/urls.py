438b8d108dd562652b47eb415dbf68822a42dc53
Anurag <anssharma61@gmail.com>
2014-03-03 20:13:29 +0530

    making the gallery

--- JHTS/urls.py
+++ JHTS/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
+    url(r'^gallery/', inclue('gallery.urls'))
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^ckeditor/', include('ckeditor.urls')),
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from gallery.models import Gallery
+from django.core.paginator import *
+from forms import GalleryForm
+from JHTS.settings import POSTS_PER_PAGE
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+	images = Gallery.objects.all()
+	paginator = Paginator(images, POSTS_PER_PAGE)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(1)
+    return render(request, 'gallery/index.html', {'images': images})
+
+def detail(request, gallery_id):
+	image = get_object_or_404(Gallery, pk=gallery_id)
+	return render(request, 'gallery/detail.html', {'image': image})
+
+@login_required
+def upload(request, template_name='gallery/form.html'):
+	if request.method == 'POST':
+        form = GalleryForm(request.POST, request.FILES)
+        if form.is_valid():
+        	form.save()
+        	messages.success(request, 'Image has been uploaded')
+        	return redirect('index')
+        return render(request, template_name, {'form': form})
+@login_required
+def delete(request, gallery_id):
+	image = get_object_or_404(Gallery, pk=gallery_id)
+	image.delete()
+	messages.success(request, 'Image has been deleted')
+	return redirect('index')
