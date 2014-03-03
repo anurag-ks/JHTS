0285804106474856c49bdb84813573d53d33ed91
Sai Vineet <saivineet89@gmail.com>
2014-03-03 13:19:47 +0530

    Aesthetics and some new UI/UX

--- JHTS/urls.py
+++ JHTS/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
     url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',{'next_page' : '/'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^ckeditor/', include('ckeditor.urls')),
     url(r'^', include('pages.urls')),
 )+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
