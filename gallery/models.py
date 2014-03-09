204c2465636fe9cc182f2ee4f8c2080b4d10d534
Anurag <anssharma61@gmail.com>
2014-03-09 17:43:42 +0530

    resizing image on upload

--- gallery/models.py
+++ gallery/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from django_resized import ResizedImageField
 
 class GalleryPhoto(models.Model):
-    image = models.ImageField(upload_to='pic_folder/')
+    image = ResizedImageField(max_width=500, max_height=300, upload_to='pic_folder/')
     description = models.CharField(max_length=50)
 
     def __unicode__(self):
