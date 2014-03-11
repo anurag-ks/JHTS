96c4ca1e5d08ec8cc2c17fde1c99ecb2f8fb3feb
Sai Vineet <saivineet89@gmail.com>
2014-03-11 21:36:11 +0530

    ABE MIRAAZ PEP8 FOLLOW KAR!
    You also mixed tabs and spaces in settings.py. Wow. :(
    I'm adding a pre-commit hook now, that should tame your horses.

--- JHTS/views.py
+++ JHTS/views.py
@@ -1,20 +1,22 @@
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 
-def signup(request, template_name="registration/signup.html"):
-	if request.method =='POST':
-		form = UserCreationForm(request.POST or None)
-		if form.is_valid():
-			form.save()
-			messages.success(request, 'User has been created, Now go to login !!')
-	        return redirect('/login/')
-		
-	else :
-		form = UserCreationForm() # unbound form
-	return render(request, template_name, {'form':form})
 
-# only works in production (DEBUG=False)
+def signup(request, template_name="registration/signup.html"):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             'User Account Created. Please proceed to login.')
+            return redirect('/login/')
+
+    else:
+        form = UserCreationForm()
+    return render(request, template_name, {'form': form})
+
+
 def error404(request):
-	render(request, "404.html")
+    render(request, "404.html")
