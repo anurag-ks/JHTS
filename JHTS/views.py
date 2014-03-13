2acf39feb5a3b9d1d98a46afb6aaf14b254544d1
Anurag <anssharma61@gmail.com>
2014-03-13 07:07:38 +0530

    added email to signup

--- JHTS/views.py
+++ JHTS/views.py
@@ -2,19 +2,29 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django import forms
 
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit = False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 def signup(request, template_name="registration/signup.html"):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST or None)
-        if form.is_valid():
-            form.save()
-            messages.success(request,
-                             'User Account Created. Please proceed to login.')
-            return redirect('/login/')
-
-    else:
-        form = UserCreationForm()
+    form = UserCreateForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request,
+                         'User Account Created. Please proceed to login.')
+        return redirect('/login/')
     return render(request, template_name, {'form': form})
 
 
