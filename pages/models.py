67ec3455e066ab42c23923e9ff3d925cc55f0c1a
Sai Vineet <saivineet89@gmail.com>
2014-03-17 14:11:23 +0530

    Made show page in navbar optional and removed comments

--- pages/models.py
+++ pages/models.py
@@ -6,7 +6,7 @@ class Page(models.Model):
     title = models.CharField(max_length=64)
     url = models.SlugField(max_length=16, unique=True)
     content = RichTextField()
-    show_comments = models.BooleanField(default=False)
+    show_in_navbar = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.title
