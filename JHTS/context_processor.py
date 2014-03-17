947715fe7083531c4a06e3d10104b44fe5fd17b6
Anurag <anssharma61@gmail.com>
2014-03-17 07:16:21 +0530

    making the slideshow

--- JHTS/context_processor.py
+++ JHTS/context_processor.py
@@ -1,5 +1,10 @@
 from pages.models import Page
+from gallery.models import GalleryPhoto
 
+def list_images(request):
+	images = GalleryPhoto.objects.all()
+	return {"images" : images}
+	
 def list_pages(request):
 	pages = Page.objects.all().order_by('-title')
 	return {"pages" : pages}
--- JHTS/settings.py
+++ JHTS/settings.py
@@ -77,6 +77,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.contrib.messages.context_processors.messages",
     "JHTS.context_processor.list_pages",
+    "JHTS.context_processor.list_images",
 )
 
 # Internationalization
